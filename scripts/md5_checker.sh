ddfc31c999369c6eb407e9d1d0da3cc4912e6bc7
Carla Cummins <carlac@ebi.ac.uk>
2020-09-08 16:21:07 +0100

    Add licence to all relevant files

--- scripts/md5_checker.sh
+++ scripts/md5_checker.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/bash
 
+# Copyright [2020] EMBL-European Bioinformatics Institute
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+
 # This script compares md5 values for the read files submitted using the covid-utils tool. Things to note:
 
 #1) input read files must have some kind of hash at the end of the filename (examples provided in script comments)
