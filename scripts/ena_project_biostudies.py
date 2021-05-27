fe08a43ddb8460256c1d903148b7a0401812da50
z-w123 <45365618+z-w123@users.noreply.github.com>
2021-05-27 18:47:14 +0100

    Update and rename ena_project_biostudies_3.py to ena_project_biostudies.py
    made changes according to pull request

--- scripts/ena_project_biostudies.py
+++ scripts/ena_project_biostudies.py
@@ -14,19 +14,66 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import requests, xmltodict
+import argparse
+import pandas as pd
+from datetime import datetime
+
+
+description = """
+Description
+-----
+This script is required for the process of assigning DOI's to ENA projects, via the registration of a Biostudies record.
+
+This script outputs a pagetab file for the (currently) manual creation of a biostudies record, which can then be assigned a DOI via contacting the Biostudies team.
+This pagetab file/biostudies record can link to a single or multiple projects. 
+
+"""
+
+usage = """
+Usage: ena_project_biostudies.py <ARGUMENTS>
+Arguments:
+    --project         : (mandatory) name of main ENA project to link to Biostudies record
+    --additional_projects   : (optional) additional project accessions to link to created Biostudies entry/ single DOI (default: none)
+    --author : (mandatory) author names to include
+    --email : (mandatory) author email addresses
+    --institution : (mandatory) institution name
+    --output_file : (optional) output file name without extension. (default: timestamp)
+"""
+
+example = """
+Example: create pagetab file for a single project with one author
+    ena_project_biostudies.py --project PRJEB37886 --author 'Zahra Waheed' --email zahra@ebi.ac.uk --institution EBI --output_file test_May_25
+
+"""
+parser = argparse.ArgumentParser(
+    description=description+usage+example
+)
+
+#parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--project', help='Main ENA project accession PRJEB#### to base Biostudies entry on', type=str, nargs='*',
+                    required=True)  # main project
+parser.add_argument('-ap', '--additional_projects', help='Additional ENA project accessions PRJEB#### to link to created Biostudies entry', type=str, nargs='*',
+                    required=False)  # additional projects
+parser.add_argument('-a', '--author', help='name of author/s separated by a space', type=str, nargs='*',
+                    required=True)  # takes in multiple authors
+parser.add_argument('-e', '--email', help='email addresses separated by a space', type=str, nargs='*',
+                    required=False)  # takes in multiple email addresses
+parser.add_argument('-i', '--institution', help='each institution name to be enclosed in quotes', type=str, nargs='*',
+                    required=True)  # takes in multiple institutes
+parser.add_argument('-o', '--output_file', help='output file name', type=str, nargs='*',
+                    required=False)  # output file
+args = parser.parse_args()
+
 
 ## retrieving project xml data (for main project only)
-####TODO: accept project as an argument#####
-import requests, xmltodict
 url_start = "https://www.ebi.ac.uk/ena/browser/api/xml"
-project_acc = "PRJEB39337" #main project accession
+project_acc = str(args.project[0])
 url = "{0}/{1}".format(url_start, project_acc)
-response = requests.get(url) #get requests retrieve the webpage for display
-data = xmltodict.parse(response.content) #this function converts the xml content into a dictionary
-#print(data)
+response = requests.get(url)  # get requests retrieve the webpage for display
+data = xmltodict.parse(response.content)  # this function converts the xml content into a dictionary
 
 ## project xml attributes:
-import datetime
 project_name = (data['PROJECT_SET']['PROJECT']['NAME'])
 
 ## to account for release date not having a fixed index in project xml:
@@ -36,7 +83,8 @@ for index, attribute in enumerate(data['PROJECT_SET']['PROJECT']['PROJECT_ATTRIB
         release_date = (data['PROJECT_SET']['PROJECT']['PROJECT_ATTRIBUTES']['PROJECT_ATTRIBUTE'][index]['VALUE'])
         print("release date is " +  release_date)
 
-#release_date = (data['PROJECT_SET']['PROJECT']['PROJECT_ATTRIBUTES']['PROJECT_ATTRIBUTE'][0]['VALUE']) #first public date does not have a fixed index
+
+
 project_title = (data['PROJECT_SET']['PROJECT']['TITLE'])
 project_description = (data['PROJECT_SET']['PROJECT']['DESCRIPTION'])
 center_name = (data['PROJECT_SET']['PROJECT']['@center_name'])
@@ -50,20 +98,6 @@ values.extend((None, project_name, release_date, ""))
 values.extend((None, project_title, project_description, center_name, ""))
 values.extend((project_accession, "Raw Data", "ENA", ""))
 
-
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('-ap', '--additional_projects', help='Additional ENA project accessions PRJEB#### to link to created Biostudies entry', type=str, nargs='*',
-                    required=False)  # additional projects
-parser.add_argument('-a', '--author', help='name of author/s separated by a space', type=str, nargs='*',
-                    required=True)  # takes in multiple authors
-parser.add_argument('-e', '--email', help='email addresses separated by a space', type=str, nargs='*',
-                    required=True)  # takes in multiple email addresses
-parser.add_argument('-i', '--institution', help='each institution name to be enclosed in quotes', type=str, nargs='*',
-                    required=True)  # takes in multiple institutes
-args = parser.parse_args()
-
-# print(args.author[1]) # how to specify which specific author or email address
 
 ## to link multiple projects to main biostudies entry
 if args.additional_projects != None:
@@ -81,16 +115,10 @@ values.extend(("<insert DOI here>", "DOI")) #this will be manually added by bios
 
 
 ## to include multiple authors in pagetab file
-
-# e.g bob, dylan, jameela
-# bob@ebi.ac.uk, o2, EBI
-# dylan@ebi.ac.uk, o3, NCBI
-# jameela@uit.ac.uk , o4, uit
-
 authors = len(args.author)
 unique_institutions = len(set(args.institution))
 
-# print(vars(args))
+
 def create_author_entries(args):
     print()
     print("adding author details below:")
@@ -106,12 +134,6 @@ def create_author_entries(args):
             print(affiliation, args.institution[i])
             ###TODO: how to print organization blocks if some authors are from the same institution?
 create_author_entries(args)
-    ##check if args.institution contains duplicate values
-    ##if yes, use the length of unique_institutions (e.g, 3) and do str(u+1) for each institution and attribute it to the corresponding author
-    #organisation block#
-#    for u in range(unique_institutions):
-#        keys.extend(("Organization", "Name", ""))
-#        values.extend(("o" + str(u + 1), args.institution[u], "")) # this is breaking the script and giving a TypeError: list indices must be integers or slices, not str
 
 
 ## creates a dataframe from zipping tuples together
@@ -119,12 +141,20 @@ create_author_entries(args)
 keys_values = list(zip(keys,values))
 print("Keys and values are:", keys_values)
 
-import pandas as pd
+
 df = pd.DataFrame(data=keys_values)
 pd.set_option("display.max_rows", None, "display.max_columns", None)
-#print()
 #print(df)
 
 
-## create the pagetab file - note csv format NOT accepted!
-df.to_excel('test20May.xlsx', header=None, index=False) #pagetab file output
+## create the pagetab file - note csv format not accepted!
+def create_output_file():
+    if args.output_file:
+        filename = str(args.output_file[0])
+        df.to_excel(f"{filename}.xlsx", header=None, index=False) #pagetab file output
+    else:
+        now = datetime.now()   # datetime object containing current date and time
+        dt_string = now.strftime("pagetab_%d-%m-%y_%H_%M_%S")
+        df.to_excel(f"{dt_string}.xlsx", header=None, index=False)  # pagetab file output
+create_output_file()
+
