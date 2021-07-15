ff02a0ca8d6198798d624b6d8ae48361bb43bf1c
z-w123 <45365618+z-w123@users.noreply.github.com>
2021-07-15 12:22:45 +0100

    now supports grouped-project DOIs
    support for grouped-project DOIs and multiple authors from the same + different institutions

--- scripts/ena_project_biostudies.py
+++ scripts/ena_project_biostudies.py
@@ -17,7 +17,8 @@
 import requests, xmltodict
 import argparse
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, date
+import sys
 
 description = """
 Description
@@ -42,8 +43,7 @@ Arguments:
 
 example = """
 Example: create pagetab file for a single project with one author
-    ena_project_biostudies.py --project PRJEB37886 --author 'Joe Bloggs' --email joe@bloggs.com --institution EBI --output_file /path/to/output_file
-
+    ena_project_biostudies.py --project PRJEB37886 --author 'Zahra Waheed' --email zahra@ebi.ac.uk --institution EBI --output_file test_May_25
 
 """
 parser = argparse.ArgumentParser(
@@ -52,106 +52,153 @@ parser = argparse.ArgumentParser(
 
 #parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--project', help='Main ENA project accession PRJEB#### to base Biostudies entry on', type=str, nargs='*',
-                    required=True)  # main project
+                    required=False)  # main project
 parser.add_argument('-ap', '--additional_projects', help='Additional ENA project accessions PRJEB#### to link to created Biostudies entry', type=str, nargs='*',
                     required=False)  # additional projects
 parser.add_argument('-a', '--author', help='name of author/s separated by a space', type=str, nargs='*',
-                    required=True)  # takes in multiple authors
+                    required=False)  # takes in multiple authors # 'Zahra Waheed'
 parser.add_argument('-e', '--email', help='email addresses separated by a space', type=str, nargs='*',
                     required=False)  # takes in multiple email addresses
 parser.add_argument('-i', '--institution', help='each institution name to be enclosed in quotes', type=str, nargs='*',
-                    required=True)  # takes in multiple institutes
+                    required=False)  # takes in multiple institutes
 parser.add_argument('-o', '--output_file', help='output file name', type=str, nargs='*',
                     required=False)  # output file
+
+
+parser.add_argument('-t', '--title', help='optional project title field for grouped Biostudies entries', type=str, nargs='*',
+                    required=False)  # project description for grouped biostudies entriess
+parser.add_argument('-d', '--description', help='optional project description field for grouped Biostudies entries', type=str, nargs='*',
+                    required=False)  # project description for grouped biostudies entriess
+parser.add_argument('-c', '--center_name', help='optional center name field for grouped Biostudies entries', type=str, nargs='*',
+                    required=False)  # center name for grouped biostudies entries
+parser.add_argument('-r', '--release_date', help='optional release date for grouped Biostudies entries', type=str, nargs='*',
+                    required=False, default=date.today().strftime("%Y-%m-%d"))  # specific release date for grouped biostudies entries, if not specified then current date taken
+
 args = parser.parse_args()
 
+##add some text/error to explain which arguements are required if you do not have a main project arguement
+
+#if single project biostudies entry then required args are: -p,
+#if grouped project biostudies entry then required args are: -ap, -t, -d, -c, -r* (quotes are required for anything longer than 1 word)
 
 ## retrieving project xml data (for main project only)
-url_start = "https://www.ebi.ac.uk/ena/browser/api/xml"
-project_acc = str(args.project[0])
-url = "{0}/{1}".format(url_start, project_acc)
-response = requests.get(url)  # get requests retrieve the webpage for display
-data = xmltodict.parse(response.content)  # this function converts the xml content into a dictionary
+if args.project:
 
-## project xml attributes:
-project_name = (data['PROJECT_SET']['PROJECT']['NAME'])
-
-## to account for release date not having a fixed index in project xml:
-for index, attribute in enumerate(data['PROJECT_SET']['PROJECT']['PROJECT_ATTRIBUTES']['PROJECT_ATTRIBUTE']):
-     if attribute['TAG'] == "ENA-FIRST-PUBLIC" :
-         #print(index, attribute)
-         release_date = (data['PROJECT_SET']['PROJECT']['PROJECT_ATTRIBUTES']['PROJECT_ATTRIBUTE'][index]['VALUE'])
-         print("release date is " +  release_date)
+    url_start = "https://www.ebi.ac.uk/ena/browser/api/xml"
+    project_acc = str(args.project[0])
+    url = "{0}/{1}".format(url_start, project_acc)
+    response = requests.get(url)  # get requests retrieve the webpage for display
+    data = xmltodict.parse(response.content)  # this function converts the xml content into a dictionary
+    project_name = (data['PROJECT_SET']['PROJECT']['NAME'])
+    project_title = (data['PROJECT_SET']['PROJECT']['TITLE'])
+    project_description = (data['PROJECT_SET']['PROJECT']['DESCRIPTION'])
+    center_name = (data['PROJECT_SET']['PROJECT']['@center_name'])
+    ######?project_accession = (data['PROJECT_SET']['PROJECT']['@accession'])
 
 
+    ## to account for release date not having a fixed index in project xml:
+    for index, attribute in enumerate(data['PROJECT_SET']['PROJECT']['PROJECT_ATTRIBUTES']['PROJECT_ATTRIBUTE']):
+        if attribute['TAG'] == "ENA-FIRST-PUBLIC":
+            #print(index, attribute)
+            release_date = (data['PROJECT_SET']['PROJECT']['PROJECT_ATTRIBUTES']['PROJECT_ATTRIBUTE'][index]['VALUE'])
+            #print("release date is " + release_date)
 
-project_title = (data['PROJECT_SET']['PROJECT']['TITLE'])
-project_description = (data['PROJECT_SET']['PROJECT']['DESCRIPTION'])
-center_name = (data['PROJECT_SET']['PROJECT']['@center_name'])
-project_accession = (data['PROJECT_SET']['PROJECT']['@accession'])
+elif args.project is None: #and (args.title or args.description or args.center_name or args.release_date):
+    project_name = args.title
+    project_title = args.title
+    project_description = args.description
+    center_name = args.center_name
+    release_date = args.release_date # this would need to be converted into string format?
 
 
 ## creating two 'key:value' style lists
-keys = ["Submission", "Title", "ReleaseDate", "", "Study", "Title", "Description", "Center Name", "", "Link", "Description", "Type", ""]
+#keys = ["Submission", "Title", "ReleaseDate", "", "Study", "Title", "Description", "Center Name", "", "Link", "Description", "Type", ""]
+keys = ["Submission", "Title", "ReleaseDate", "", "Study", "Title", "Description", "Center Name", ""]
 values = []
 values.extend((None, project_name, release_date, ""))
 values.extend((None, project_title, project_description, center_name, ""))
-values.extend((project_accession, "Raw Data", "ENA", ""))
 
 
-## to link multiple projects to main biostudies entry
-if args.additional_projects != None:
+## linking ENA projects (either 1 or several) to biostudies entry
+def link_projects(args):
+    if args.additional_projects is None:
+        project_accession = (data['PROJECT_SET']['PROJECT']['@accession'])
+        keys.extend(("Link", "Description", "Type", ""))
+        values.extend((project_accession, "Raw Data", "ENA", ""))
+    elif args.additional_projects:
+        additional_projects = len(args.additional_projects)
+        for a_project in range(additional_projects):
+            keys.extend(("Link", "Description", "Type", ""))
+            values.extend((args.additional_projects[a_project], "Raw Data", "ENA", ""))
+link_projects(args)
 
-     additional_projects = len(args.additional_projects)
 
-     def link_additional_projects(args): #not a required arguement
-         for a_project in range(additional_projects):
-             keys.extend(("Link", "Description", "Type", ""))
-             values.extend((args.additional_projects[a_project], "Raw Data", "ENA", ""))
-     link_additional_projects(args)
-
-keys.extend(("Link", "Type"))
-values.extend(("<insert DOI here>", "DOI")) #this will be manually added by biostudies team
+#email addresses for each author not necessary.
+#but at least 1 corresponding author with email address
+#but each author should have an institutional affiliation
+## first author details supplied to script should be corresponding author's
+def corresponding_author(args): #max details for corresponding author: name, email, affiliation, institution
+    print("corresponding author details:")
+    keys.extend(("Author", "Name", "Email", "<affiliation>", ""))
+    values.extend(("", args.author[0], args.email[0], "o1"))
+    print("", "", args.author[0], args.email[0])
+corresponding_author(args)
 
 
 ## to include multiple authors in pagetab file
 authors = len(args.author)
+other_authors = args.author[1:] #omitting corresponding author from author list
+len_oa = len(args.author[1:])
+#print(other_authors[0])
 unique_institutions = len(set(args.institution))
 unique_institutions_list = list(set(args.institution))
 
-def create_author_entries(args):
-    print()
-    print("adding author details below:")
-    for i in range(authors):
-        keys.extend(("", "Author", "Name", "Email", "<affiliation>", ""))
-        values.extend(("", "", args.author[i], args.email[i]))
-        print("", args.author[i], args.email[i])
-        if authors == unique_institutions:
-            affiliation = "o" + str(i + 1)
-            values.extend((affiliation, "")) #value for author 'affiliation' key
-            keys.extend(("Organization", "Name"))
-            values.extend((affiliation, args.institution[i]))
-            print(affiliation, args.institution[i])
-        else: #printing affiliation if multiple authors are from the same institution
-            for institute in unique_institutions_list:
-                affiliation = "o" + str(unique_institutions_list.index(institute) + 1)
-            values.extend((affiliation, ""))  # value for author 'affiliation' key
-    keys.extend(("Organization", "Name"))
-    values.extend((affiliation, args.institution[i]))
-    print(affiliation, args.institution[i])
-create_author_entries(args)
+print(unique_institutions_list)
 
+
+## equating each institution to it's own affiliation string and storing this in a list
+affiliations = []
+for inst in args.institution:
+    affn = "o" + str(args.institution.index(inst) + 1)
+    affiliations.append(affn)
+#print(affiliations)
+#print(affiliations[1:])
+other_affns = affiliations[1:] #omitting corresponding author's affiliation
+#print(affiliation)
+
+## adding minimal details for remaining author list: name, affiliation
+def add_author_list(args): #minimal details for author list:
+    for i in range(len(other_authors)): #omitting corresponding author details
+        if authors == unique_institutions:
+            affiliation = "o" + str(i + 2) #'frameshifting' for corresponding author
+            keys.extend(("Author", "Name", "<affiliation>", ""))  # no email necessary here
+            values.extend(("", "", other_authors[i], affiliation))
+        else:
+            keys.extend(("Author", "Name", "<affiliation>", ""))  # no email necessary here
+            values.extend(("", "", other_authors[i], other_affns[i])) #if multiple authors are from the same institution
+add_author_list(args)
+
+
+## adding final organisation block
+def organisation_block(args):
+    for name in unique_institutions_list:
+        keys.extend(("Organization", "Name", ""))
+        values.extend(("", "o" + str(unique_institutions_list.index(name) + 1), name)) #NOT alphabetically ordered
+organisation_block(args)
 
 
 ## creates a dataframe from zipping tuples together
+print(keys)
+print(values)
 
-keys_values = list(zip(keys,values))
-print("Keys and values are:", keys_values)
+keys_values = zip(keys,values)
+# keys_values = list(zip(keys,values)) #list function changes formatting of organisation block above - it wrongly reorders the list of unique institutions alphabetically and thus wrongly assigns the 'o1', 'o2' etc. affiliations
+# print("Keys and values are:", list(keys_values))
 
 
 df = pd.DataFrame(data=keys_values)
 pd.set_option("display.max_rows", None, "display.max_columns", None)
-#print(df)
+print(df)
 
 
 ## create the pagetab file - note csv format not accepted!
@@ -164,3 +211,19 @@ def create_output_file():
         dt_string = now.strftime("pagetab_%d-%m-%y_%H_%M_%S")
         df.to_excel(f"{dt_string}.xlsx", header=None, index=False)  # pagetab file output
 create_output_file()
+
+
+
+###TODO: convert input to json format
+
+
+
+
+
+
+
+
+
+
+
+
