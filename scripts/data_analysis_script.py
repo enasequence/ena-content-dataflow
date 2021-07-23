cf526166071c312541d85a3ccdecf8425769fec6
Ahmad Zyoud <43208623+ahmadazd@users.noreply.github.com>
2021-07-23 15:21:19 +0100

    Create data_analysis_script.py
    This script is the second part of the automation process for data flow error reporting between NCBI and ENA. This script works to analyse the data that been fetched and retrieved by the data_fetch_script.py
    
    Features:
    Creates reports for data missing from ENA advance search comparing NCBI.
    
    Create Report for data missing from COVID-19 data portal comparing to ENA advance search
    
    Find the duplicates if present in COVID-19 data portal
    
    Compare data from EBI search and ENA advance search.

---
+++ scripts/data_analysis_script.py
@@ -0,0 +1,223 @@
+#!/usr/bin/env python3.8
+
+# Copyright [2020] EMBL-European Bioinformatics Institute
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import argparse, hashlib, os, subprocess, sys, time
+import sys
+import shlex
+import subprocess
+import requests, sys
+import re
+import requests
+import json
+from datetime import datetime
+import cx_Oracle
+from getpass import getpass
+import collections
+import numpy as np
+import pandas as pd
+parser = argparse.ArgumentParser(prog='data_analysis_script.py', formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     epilog="""
+        + ============================================================ +
+        |  European Nucleotide Archive (ENA) data flow monitoring Tool |
+        |                                                              |
+        |   Tool to to analyse and compare data from NCBI or ENA       |
+        + =========================================================== +
+This script is used to analyse the data from NCBI and ENA that been produced by data_fetch_script.py.        
+        """)
+parser.add_argument('-f', '--file', help='path for the input files', type=str, required=True)
+args = parser.parse_args()
+
+def get_oracle_usr_pwd():
+    usr = input("Username: ")
+    pwd = getpass()
+    return usr, pwd
+
+def setup_connection():
+    oracle_usr, oracle_pwd = get_oracle_usr_pwd()
+    client_lib_dir = os.getenv('ORACLE_CLIENT_LIB')
+    if database == 'sequences':
+        if not client_lib_dir or not os.path.isdir(client_lib_dir):
+            sys.stderr.write("ERROR: Environment variable $ORACLE_CLIENT_LIB must point at a valid directory\n")
+            exit(1)
+        cx_Oracle.init_oracle_client(lib_dir=client_lib_dir)
+        connection = None
+        try:
+            dsn = cx_Oracle.makedsn("ora-vm5-008.ebi.ac.uk", 1531, service_name="ENAPRO")
+            connection = cx_Oracle.connect(oracle_usr, oracle_pwd, dsn, encoding="UTF-8")
+            return connection
+        except cx_Oracle.Error as error:
+            print(error)
+    else:
+        if not client_lib_dir or not os.path.isdir(client_lib_dir):
+            sys.stderr.write("ERROR: Environment variable $ORACLE_CLIENT_LIB must point at a valid directory\n")
+            exit(1)
+        cx_Oracle.init_oracle_client(lib_dir=client_lib_dir)
+        connection = None
+        try:
+            dsn = cx_Oracle.makedsn("ora-vm-009.ebi.ac.uk", 1541, service_name="ERAPRO")
+            connection = cx_Oracle.connect(oracle_usr, oracle_pwd, dsn, encoding="UTF-8")
+            return connection
+        except cx_Oracle.Error as error:
+            print(error)
+
+def fetch_and_filter_seq(connection, output):
+    c = connection.cursor()
+    f = open(f"{args.file}/analysis.inENAPRO.sequences.log.txt", "w")
+    header = "\t".join(['Accession', 'First_public', 'Last_public', 'status_id' ])
+    f.write(str(header) + "\n")
+    accession_list_seq = []
+    for accession in output:
+        c.execute(f"select TRUNC(first_public), TRUNC(last_public), statusid from dbentry where primaryacc# in ('{accession}')")
+        for row in c:
+            f.write(str(accession) + "\t" + str(row[0]) + "\t" + str(row[1]) + "\t" + str(row[2]) + "\n" )
+            accession_list_seq.append(accession)
+    f.close()
+    print('Data Processing............')
+    noENAPRO_f = open(f"{args.file}/analysis.NoENAPRO.sequences.log.txt", "w")
+    accession_set_seq=set(accession_list_seq)
+    accession_set_seq_diff = output.difference(accession_set_seq)
+    no_enapro_list= [acc for acc in accession_set_seq_diff ]
+    release_date_list = []
+    for i in range(0, len(no_enapro_list), 100):
+        stripped_list = ', '.join(no_enapro_list[i:i + 100])
+        command = 'esearch -db nucleotide -query "{}" |   efetch -format docsum |xtract -pattern DocumentSummary -element Caption, UpdateDate'.format(stripped_list)
+        sp = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = sp.communicate()
+        stdoutOrigin = sys.stdout
+        release_date = out.decode().strip("\n").split("\n")
+        for obj in release_date:
+            release_date_list.append(obj)
+        sys.stdout = stdoutOrigin
+    noENAPRO_f.write("\n".join(release_date_list) + "\n")
+    noENAPRO_f.close()
+    # conn.close()
+def fetch_and_filter_reads(connection, output):
+    c = connection.cursor()
+    f = open(f"{args.file}/analysis.inERAPRO.reads.log.txt", "w")
+    header = "\t".join(['Accession', 'Run_id', 'Status_id' 'First_public', 'Last_updated'])
+    f.write(str(header) + "\n")
+    accession_list_reads = []
+    for accession in output:
+        c.execute(
+            f"select run_id, status_id, first_public, last_updated from run where experiment_id in ('{accession}')")
+        for row in c:
+            f.write(str(accession) + "\t" + str(row[0]) + "\t" + str(row[1]) + "\t" + str(row[2]) + str(row[3]) + "\n")
+            accession_list_reads.append(accession)
+    f.close()
+    print('Data Processing...........')
+    accession_set_reads=set(accession_list_reads)
+    accession_set_reads_diff = output.difference(accession_set_reads)
+    noERAPRO_df= pd.DataFrame(list(accession_set_reads_diff), columns=['experiment_id'])
+
+    inner_join = pd.merge(noERAPRO_df, sra_df, on='experiment_id', how='inner')
+    inner_join.to_csv(f"{args.file}/analysis.NoERAPRO.reads.log.txt")
+
+def reads_dataset_difference ():
+    output = set(sra_df.experiment_id).difference(set(ena_read_df.experiment_id))
+    length_read_set = len(output)
+
+    f = open(f"{args.file}/'NCBI_vs_ENA_'{database}.log.txt", "w")
+    values = "\n".join(map(str, list(output)))
+    f.write(values)
+    f.close()
+    print('Number of data found in NCBI ( SRA database) but missing in ENA advance search is: ', length_read_set)
+    return output
+def sequence_dataset_difference ():
+    output = ncbivirus_set.difference(ena_seq_set)
+    length_seq_set = len(output)
+    f = open(f"{args.file}/'NCBI_vs_ENA_'{database}.log.txt", "w")
+    f.write("\n".join(output) + "\n")
+    f.close()
+    print('Number of data found in NCBI ( NCBIVirus database) but missing in ENA advance search is: ', length_seq_set)
+    return output
+def covid_advance_search_difference (ena_dataset, covid_portal_set, covid_portal_list):
+    # Obtain the reads difference between Advanced search and COVID-19 Portal
+    covid_portal_output = set(ena_dataset).difference(covid_portal_set)
+    covid_diff_leng_set = len(covid_portal_output)
+    f = open(f"{args.file}/Covid19Portal.vs.ENA.advance.{database}.log.txt", "w")
+    f.write("\n".join(covid_portal_output) + "\n")
+    f.close()
+    print(f"Number of {database} found in ENA advance search but missing in COVID-19 data portal is: ", covid_diff_leng_set)
+
+    # to create a list of reads duplicates if present in COVID-19 data Portal
+    f_duplicates = open(f"{args.file}/Duplicates.Covid19Portal.{database}.log.txt", "w")
+    covid_duplicate_list= []
+    for item, count in collections.Counter(covid_portal_list).items():
+        if count > 1:
+            f_duplicates.write(str(item) + "\n")
+            covid_duplicate_list.append(item)
+    f_duplicates.close()
+    length_covid_duplicate = len(covid_duplicate_list)
+    print(f"Number of {database} found duplicated in COVID-19 data portal is: ", length_covid_duplicate)
+
+#############
+##  MAIN   ##
+#############
+database = input("please indicate the dataset type, ex: sequences or reads: ").lower()
+
+
+if database == 'reads':
+    #Connecting to ERAPRO
+    sys.stderr.write("Connecting to ERAPRO...\n")
+    db_conn = setup_connection()
+
+    #Uploading Files from NCBI and ENA
+    sra_df =pd.read_csv(f"{args.file}/NCBI.sra.log.txt", sep="\t", header=None, names =['run_id', 'experiment_id', 'release_date'])
+    print ('Number of Reads in NCBI (SRA database) is: ', len(sra_df))
+    ena_read_df = pd.read_csv(f"{args.file}/ENA.read_experiment.log.txt", sep="\t", header=None, names =['experiment_id'])
+    print('Number of Reads in ENA Advance Search is: ', len(ena_read_df))
+    #Uploading files from COVID-19 data Portal
+    covid_reads_portal_list = list(open(f"{args.file}/Covid19DataPortal.raw-reads.log.txt").read().split())
+    covid_reads_portal_set = set(covid_reads_portal_list)
+    print('Number of Reads in COVID-19 data portal is: ', len(covid_reads_portal_list))
+
+    #Obtain the difference between the data in NCBI and ENA
+    output = reads_dataset_difference()
+
+    #Obtain the reads difference between Advanced search and COVID-19 Portal, and duplicates if present
+    covid_advance_search_difference(ena_read_df, covid_reads_portal_set, covid_reads_portal_list)
+
+    #Querying ERAPRO
+    sys.stderr.write("Querying ERAPRO ..........\n")
+    fetch_and_filter_reads(db_conn, output)
+
+elif database == 'sequences':
+    #Connecting to ENAPRO
+    sys.stderr.write("Connecting to ENAPRO...\n")
+    db_conn = setup_connection()
+
+    # Uploading Files from NCBI and ENA
+    ncbivirus_set = set(open(f"{args.file}/NCBI.ncbivirus.log.txt").read().split())
+    print('Number of Sequences in NCBI (NCBIVirus database) is: ', len(ncbivirus_set))
+    ena_seq_set = set(open(f"{args.file}/ENA.sequence.log.txt").read().split())
+    print('Number of Sequences in ENA Advance Search is: ', len(ena_seq_set))
+    # Uploading files from COVID-19 data Portal
+    covid_seq_portal_list = list(open(f"{args.file}/Covid19DataPortal.sequences.log.txt").read().split())
+    covid_seq_portal_set = set(covid_seq_portal_list)
+    print('Number of Sequences in COVID-19 data portal is: ', len(covid_seq_portal_list))
+
+    #Obtain the difference between the data in NCBI and ENA
+    output = sequence_dataset_difference()
+
+    #Obtain the sequence difference between Advanced search and COVID-19 Portal, and duplicates if present
+    covid_advance_search_difference(ena_seq_set,covid_seq_portal_set, covid_seq_portal_list)
+
+    #Querying ENAPRO
+    sys.stderr.write("Querying ENAPRO ..........\n")
+    fetch_and_filter_seq(db_conn, output)
+else:
+    print('The dataset type does not exist, please check your spelling and try again')
+
+print ("\n **************** END ***************")
