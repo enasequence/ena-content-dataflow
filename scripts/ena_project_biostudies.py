6d2da59d77f6d07101394e9d5f8ddb09867689be
z-w123 <45365618+z-w123@users.noreply.github.com>
2021-07-26 09:47:30 +0100

    converted argparse options to dict + addressed comments in PR#5

--- scripts/ena_project_biostudies.py
+++ scripts/ena_project_biostudies.py
@@ -42,9 +42,21 @@ Arguments:
 """
 
 example = """
-Example: create pagetab file for a single project with one author
-    ena_project_biostudies.py --project PRJEB37886 --author 'Zahra Waheed' --email zahra@ebi.ac.uk --institution EBI --output_file test_May_25
+Single Project based pagetabs:
+    Example: create pagetab file for a single project with one author
+    
+    python ena_project_biostudies.py --project PRJEB37886 --author 'Joe Bloggs' --email joe@bloggs.ac.uk --institution EBI --output_file test_May_25
 
+Grouped Project pagetabs:
+    Example: create pagetab file for a grouped project with one corresponding author
+    
+    mandatory arguments = -ap, -t, -d, -c, -r* (current date is taken if -r not provided)
+    
+    python ena_project_biostudies.py -ap PRJEB402 PRJEB1787 -t 'Grouped Project Title' -d 'this is a grouped description' -c 'new center name' -a 'Joe Bloggs' -e joe@bloggs.ac.uk -i CNN
+
+    Example 2: create pagetab file for a grouped project with several authors:
+    
+    python ena_project_biostudies.py -ap PRJEB402 PRJEB1787 -t 'Grouped Project Title' -d 'this is a grouped description' -c 'new center name' -a 'Joe Bloggs' 'Jenny Smith' 'Jane Doe' -e joe@bloggs.ac.uk -i CNN BBC SkyNews
 """
 parser = argparse.ArgumentParser(
     description=description+usage+example
@@ -56,11 +68,11 @@ parser.add_argument('-p', '--project', help='Main ENA project accession PRJEB###
 parser.add_argument('-ap', '--additional_projects', help='Additional ENA project accessions PRJEB#### to link to created Biostudies entry', type=str, nargs='*',
                     required=False)  # additional projects
 parser.add_argument('-a', '--author', help='name of author/s separated by a space', type=str, nargs='*',
-                    required=False)  # takes in multiple authors # 'Zahra Waheed'
+                    required=True)  # takes in multiple authors
 parser.add_argument('-e', '--email', help='email addresses separated by a space', type=str, nargs='*',
-                    required=False)  # takes in multiple email addresses
+                    required=True)  # takes in multiple email addresses
 parser.add_argument('-i', '--institution', help='each institution name to be enclosed in quotes', type=str, nargs='*',
-                    required=False)  # takes in multiple institutes
+                    required=True)  # takes in multiple institutes
 parser.add_argument('-o', '--output_file', help='output file name', type=str, nargs='*',
                     required=False)  # output file
 
@@ -75,17 +87,19 @@ parser.add_argument('-r', '--release_date', help='optional release date for grou
                     required=False, default=date.today().strftime("%Y-%m-%d"))  # specific release date for grouped biostudies entries, if not specified then current date taken
 
 args = parser.parse_args()
+args_dict = vars(args) #vars() to convert argparse options to a dictionary
+
 
 ##add some text/error to explain which arguements are required if you do not have a main project arguement
 
 #if single project biostudies entry then required args are: -p,
 #if grouped project biostudies entry then required args are: -ap, -t, -d, -c, -r* (quotes are required for anything longer than 1 word)
 
-## retrieving project xml data (for main project only)
-if args.project:
+# retrieving project xml data (for main project only)
+if args_dict['project']:
 
     url_start = "https://www.ebi.ac.uk/ena/browser/api/xml"
-    project_acc = str(args.project[0])
+    project_acc = str(args_dict['project'][0])
     url = "{0}/{1}".format(url_start, project_acc)
     response = requests.get(url)  # get requests retrieve the webpage for display
     data = xmltodict.parse(response.content)  # this function converts the xml content into a dictionary
@@ -93,7 +107,6 @@ if args.project:
     project_title = (data['PROJECT_SET']['PROJECT']['TITLE'])
     project_description = (data['PROJECT_SET']['PROJECT']['DESCRIPTION'])
     center_name = (data['PROJECT_SET']['PROJECT']['@center_name'])
-    ######?project_accession = (data['PROJECT_SET']['PROJECT']['@accession'])
 
 
     ## to account for release date not having a fixed index in project xml:
@@ -103,16 +116,15 @@ if args.project:
             release_date = (data['PROJECT_SET']['PROJECT']['PROJECT_ATTRIBUTES']['PROJECT_ATTRIBUTE'][index]['VALUE'])
             #print("release date is " + release_date)
 
-elif args.project is None: #and (args.title or args.description or args.center_name or args.release_date):
-    project_name = args.title
-    project_title = args.title
-    project_description = args.description
-    center_name = args.center_name
-    release_date = args.release_date # this would need to be converted into string format?
+elif args_dict['project'] is None: #and (args.title or args.description or args.center_name or args.release_date):
+    project_name = str(args_dict['title'][0])
+    project_title = str(args_dict['title'][0])
+    project_description = str(args_dict['description'][0])
+    center_name = str(args_dict['center_name'][0])
+    release_date = str(args_dict['release_date'])
 
 
 ## creating two 'key:value' style lists
-#keys = ["Submission", "Title", "ReleaseDate", "", "Study", "Title", "Description", "Center Name", "", "Link", "Description", "Type", ""]
 keys = ["Submission", "Title", "ReleaseDate", "", "Study", "Title", "Description", "Center Name", ""]
 values = []
 values.extend((None, project_name, release_date, ""))
@@ -121,17 +133,16 @@ values.extend((None, project_title, project_description, center_name, ""))
 
 ## linking ENA projects (either 1 or several) to biostudies entry
 def link_projects(args):
-    if args.additional_projects is None:
+    if args_dict['additional_projects'] is None:
         project_accession = (data['PROJECT_SET']['PROJECT']['@accession'])
         keys.extend(("Link", "Description", "Type", ""))
         values.extend((project_accession, "Raw Data", "ENA", ""))
-    elif args.additional_projects:
-        additional_projects = len(args.additional_projects)
+    elif args_dict['additional_projects']:
+        additional_projects = len(args_dict['additional_projects'])
         for a_project in range(additional_projects):
             keys.extend(("Link", "Description", "Type", ""))
-            values.extend((args.additional_projects[a_project], "Raw Data", "ENA", ""))
+            values.extend((args_dict['additional_projects'][a_project], "Raw Data", "ENA", ""))
 link_projects(args)
-
 
 #email addresses for each author not necessary.
 #but at least 1 corresponding author with email address
@@ -140,31 +151,26 @@ link_projects(args)
 def corresponding_author(args): #max details for corresponding author: name, email, affiliation, institution
     print("corresponding author details:")
     keys.extend(("Author", "Name", "Email", "<affiliation>", ""))
-    values.extend(("", args.author[0], args.email[0], "o1"))
-    print("", "", args.author[0], args.email[0])
+    values.extend(("", args_dict['author'][0], args_dict['email'][0], "o1"))
+    print("", "", args_dict['author'][0], args_dict['email'][0])
 corresponding_author(args)
 
 
 ## to include multiple authors in pagetab file
-authors = len(args.author)
-other_authors = args.author[1:] #omitting corresponding author from author list
-len_oa = len(args.author[1:])
-#print(other_authors[0])
-unique_institutions = len(set(args.institution))
-unique_institutions_list = list(set(args.institution))
+authors = len(args_dict['author'])
+other_authors = args_dict['author'][1:] #omitting corresponding author from author list
 
-print(unique_institutions_list)
+unique_institutions = len(set(args_dict['institution']))
+unique_institutions_list = list(set(args_dict['institution']))
 
 
 ## equating each institution to it's own affiliation string and storing this in a list
 affiliations = []
-for inst in args.institution:
-    affn = "o" + str(args.institution.index(inst) + 1)
+for inst in args_dict['institution']:
+    affn = "o" + str(args_dict['institution'].index(inst) + 1)
     affiliations.append(affn)
-#print(affiliations)
-#print(affiliations[1:])
+
 other_affns = affiliations[1:] #omitting corresponding author's affiliation
-#print(affiliation)
 
 ## adding minimal details for remaining author list: name, affiliation
 def add_author_list(args): #minimal details for author list:
@@ -193,7 +199,7 @@ print(values)
 
 keys_values = zip(keys,values)
 # keys_values = list(zip(keys,values)) #list function changes formatting of organisation block above - it wrongly reorders the list of unique institutions alphabetically and thus wrongly assigns the 'o1', 'o2' etc. affiliations
-# print("Keys and values are:", list(keys_values))
+
 
 
 df = pd.DataFrame(data=keys_values)
@@ -203,27 +209,14 @@ print(df)
 
 ## create the pagetab file - note csv format not accepted!
 def create_output_file():
-    if args.output_file:
-        filename = str(args.output_file[0])
+    if args_dict['output_file']:
+        filename = str(args_dict['output_file'][0])
         df.to_excel(f"{filename}.xlsx", header=None, index=False) #pagetab file output
     else:
         now = datetime.now()   # datetime object containing current date and time
         dt_string = now.strftime("pagetab_%d-%m-%y_%H_%M_%S")
         df.to_excel(f"{dt_string}.xlsx", header=None, index=False)  # pagetab file output
 create_output_file()
-
-
-
-###TODO: convert input to json format
-
-
-
-
-
-
-
-
-
 
 
 
