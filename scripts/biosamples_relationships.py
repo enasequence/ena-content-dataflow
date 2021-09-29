f82b6722600c8d9e98f898e26d77feed0e430b77
z-w123 <45365618+z-w123@users.noreply.github.com>
2021-09-29 09:55:49 +0100

    Update biosamples_relationships.py
    removed webinSubmissionAccountId (already present + set to correct webin in original sample json)
    supports export of webin credentials as environment variables

--- scripts/biosamples_relationships.py
+++ scripts/biosamples_relationships.py
@@ -23,6 +23,7 @@ from datetime import datetime, date
 import os
 from argparse import RawTextHelpFormatter
 
+
 description = """
 This script links source (e.g. ENA) biosamples with target (e.g. EGA) biosamples, using the "derived from" relationships field
 The script currently supports a 1:1 linking of source to target biosample
@@ -38,7 +39,6 @@ Options:
 example = """
 Example 1: link biosamples in development environment:
     python3 biosamples_relationships.py -s test_sample_accs.txt
-
 Example 2: link biosamples in production:
     python3 biosamples_relationships.py -s test_sample_accs.txt -prod
 """
@@ -52,14 +52,11 @@ parser.add_argument('-s', '--spreadsheet', help='input spreadsheet containing so
 parser.add_argument('-prod', '--production', help='Biosamples production environment', action='store_true')  # 'dev' env is default if prod not specified
 args = parser.parse_args()
 
-##TODO: Dipayan mentioned adding the "webinSubmissionAccountId" in the sample json when updating it to retain original owner of sample - to be fixed by mid-Aug
 
-#TODO: how can we connect this script to the creation of the ENA 'sample derived from' attribute?
-
-root_user = input("Enter the root username: ")
-print(root_user)
-root_pwd = input("Enter the root password: ")
-print(root_pwd)
+root_user = os.environ['root_user']
+root_pwd = os.environ['root_pwd']
+#print(root_user)
+#print(root_pwd)
 
 # creating output dir
 def prepare_directory():
@@ -80,11 +77,13 @@ print(data)
 
 response = requests.post(auth_url, headers=header, data=data)
 token = response.content.decode('utf-8') #to decode from bytes (indicated by b') to string
-print("Token is " + token)
 
-if response.status_code != 200:
+if response.status_code == 200:
+    print("Token is " + token)
+
+elif response.status_code != 200:
     print("\n Error generating token, see below:")
-    print(response.content)
+    print(response.content.decode('utf-8'))
     sys.exit()
 
 # input list of source + target accessions:
@@ -120,11 +119,8 @@ for i in range(len(df)):
         #print(file_data)
     file_data.pop("_links") #removes links array (will be added automatically after updating the biosample)
      #note the below code assumes a 1:1 mapping between ENA:EGA biosamples
-    array1 = {"relationships": [{"source": ena_bs, "type": "derived from", "target": ega_bs}]}  #.tolist() preserves order, so mapping between ENA->EGA biosample accession should also be preserved
-    array2 = {"webinSubmissionAccountId": root_user} #including this field in sample json allows original submitter to retain ownership of sample
-    #print('Source sample: ' + ena_bs + ' derived from target sample: ' + ega_bs)
-    newarray = {**array1, **array2}
-    file_data.update(newarray)
+    array = {"relationships": [{"source": ena_bs, "type": "derived from", "target": ega_bs}]}  #.tolist() preserves order, so mapping between ENA->EGA biosample accession should also be preserved
+    file_data.update(array)
 
     with open(os.path.join(path, f"linked_{ena_bs}.json"), 'w') as f:
         json.dump(file_data, f, indent=1) #converts python dictionary back into json string
