e4c4bb82fd07c6da8b95127cb8b2e8eb21c40c79
Ahmad Zyoud <43208623+ahmadazd@users.noreply.github.com>
2021-09-30 16:21:33 +0100

    Update data_fetch_script.py
    looping through the months to fetch data from ebisearch and covid19 data portal.

--- scripts/data_fetch_script.py
+++ scripts/data_fetch_script.py
@@ -141,6 +141,7 @@ def ebisearch_data_fetching(database):
     print('PROCESSING DATA FROM EBI SEARCH...................................................................')
 
     # Using While loop to go through all the pages in the ebisearch API
+<<<<<<< Updated upstream
     start = 0
     while start >= 0:
         server = "http://www.ebi.ac.uk/ebisearch/ws/rest"
@@ -164,6 +165,38 @@ def ebisearch_data_fetching(database):
                 output = x["id"]
                 f.write(output + "\n")
             f.close()
+=======
+    now = datetime.datetime.now()
+    start = datetime.datetime(2020, 1, 1)
+    delta = relativedelta(months=1)
+    while start <= now:
+        start_item = 0
+        year=start.strftime("%Y")
+        month =start.strftime("%m")
+        start += delta
+        while start_item >= 0:
+            server = "http://www.ebi.ac.uk/ebisearch/ws/rest"
+            ext = "/{}?query=TAXON:{} AND {}:{}-{}&fields=acc,{}&format=json&size=1000&start={}".format(database, tax_fetch[0],date,year,month, date, start_item)
+            start_item = start_item + 1000
+            command = requests.get(server + ext, headers={"Content-Type": "application/json"})
+            status = command.status_code
+            if status in [400, 500]:
+                if status == 500:
+                    sys.stderr.write(
+                        "Attention: Internal Server Error, the process has stopped and skipped ( Data might be incomplete )\n")
+                break
+            else:
+                data = json.loads(command.content)
+                jsonData = data["entries"]
+                if start == '2020-02-01 00:00:00':
+                    f = open(f"{outdir}/{'EBIsearch'}.{database}.log.txt", "w")
+                else:
+                    f = open(f"{outdir}/{'EBIsearch'}.{database}.log.txt", "a")
+                for x in jsonData:
+                    output = [x['fields']['acc'][0], x['fields'][date][0]]
+                    f.write("\t".join(output) + "\n")
+                f.close()
+>>>>>>> Stashed changes
     print('EBI-Search Data written to ' + f"{outdir}/{'ENA'}.{database}.log.txt")
 
 
