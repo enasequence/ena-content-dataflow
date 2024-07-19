73c62b92af51921658cdd607249dd3e23791c7a9
Jasmine Elaine Mckinnon <58950226+jas-mckin@users.noreply.github.com>
2024-07-19 11:12:37 +0100

    updates to assembly tracking scripts
    adds argaparser
    puts scripts under if name = main fucnction
    possible to run externally to python interpreter

--- scripts/assemblytracking/step1_add_assemblies_to_file.py
+++ scripts/assemblytracking/step1_add_assemblies_to_file.py
@@ -14,40 +14,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import io
-import os
-import requests
+import io, os, sys, argparse, requests
 import numpy as np
 import pandas as pd
 from pandas import json_normalize
 
-##################
-##  USER INPUT  ##
-##################
-# set the working directory
-# check the current working directory
- # should be 'C:\\Users\\USERNAME\\pathto\\githubrepo\\ena-content-dataflow' on local machine
-# set thw working directory to location of scripts and of config file
-
-# set which project to track - determines the folder where tracking files will be read and written
-  # DToL or ASG or ERGA
-
 
 parser = argparse.ArgumentParser(description="sql_processingatENA")
 parser.add_argument('-p', '--project', help="Project to track DToL, ASG or ERGA", default="none")
-parser.add_argument('-c', '--config', help="config file path", default="config_private.yaml")
 parser.add_argument('-w', '--workingdir', help="location of tracking file folders",
                                                 default="scripts/assemblytracking/")
 opts = parser.parse_args()
-
-
-###################
-##  FILE INPUTS  ##
-###################
-# (optional - from initial import script) to  import list of public assemblies from another source in excel format
-#data  =  pd.read_excel('ERGA_Publicly_available_04Jan23.xlsx', sheet_name='Publicly available')
-# rename column names to 'dataset' format
-#dataset = pd.DataFrame(data, columns=['name', 'submission date', 'accessioned', 'shared to NCBI', 'project', 'analysis ID', 'sample ID', 'GCA ID', 'Contig range', 'Chr range','Assembly type'])
 
 
 #############
@@ -55,106 +32,114 @@ opts = parser.parse_args()
 #############
 # Purpose of script - adds new assemblies to the tracking_file.txt in the format used for logging and tracking of
 # the progress of the assemblies release.
-# add if __name__ == "__main__":
-#TODO: tidy this script into functions:
+
+# TODO: tidy this script into functions:
 # 1- get Taxon info and report API errors.
 # 2 - add to tracking file and indexing - look at improving indexing.
 
-# set thw working directory to location of scripts and of config file
-os.chdir(opts.workingdir)
-# set which project to track - determines the folder where tracking files will be read and written
-project = opts.project  # DToL or ASG or ERGA
-# set the location of the tracking files
-tracking_files_path = f'{project}-tracking-files'
-tracking_file_path = f'{tracking_files_path}/tracking_file.txt'
-# import tracking file
-tracking = pd.read_csv(tracking_file_path, sep='\t',index_col=0)
-# import file with new accessions to add to tracking file
-data = pd.read_csv(f'{tracking_files_path}/Releasing_sequences.txt', sep='\t', header=True)
+if __name__ == "__main__":
+
+    # set thw working directory to location of scripts and of config file
+    os.chdir(opts.workingdir)
+    # set which project to track - determines the folder where tracking files will be read and written
+    project = opts.project  # DToL or ASG or ERGA
+    # set the location of the tracking files
+    tracking_files_path = f'{project}-tracking-files'
+    tracking_file_path = f'{tracking_files_path}/tracking_file.txt'
+    ###################
+    ##  FILE INPUTS  ##
+    ###################
+    # (optional - from initial import script) to  import list of public assemblies from another source in excel format
+    # data  =  pd.read_excel('ERGA_Publicly_available_04Jan23.xlsx', sheet_name='Publicly available')
+    # rename column names to 'dataset' format
+    # dataset = pd.DataFrame(data, columns=['name', 'submission date', 'accessioned', 'shared to NCBI', 'project', 'analysis ID', 'sample ID', 'GCA ID', 'Contig range', 'Chr range','Assembly type'])
+
+    # import tracking file
+    tracking = pd.read_csv(tracking_file_path, sep='\t',index_col=0)
+    # import file with new accessions to add to tracking file
+    data = pd.read_csv(f'{tracking_files_path}/Releasing_sequences.txt', sep='\t', header=0)
 
 
-# get index of the latest row in the tracking file and increment this index onto new assemblies to be added to tracking
-last_index = tracking['index'].iloc[-1]
-# subset input data by slicing dataframe
-dataset = data.iloc[:, 0:11] # dataset = data.loc[:,'name':'Assembly type'] #this also works
-# apply index to new accessions dataset
-dataset.index = dataset.index + last_index + 1
+    # get index of the latest row in the tracking file and increment this index onto new assemblies to be added to tracking
+    last_index = tracking['index'].iloc[-1]
+    # subset input data by slicing dataframe
+    dataset = data.iloc[:, 0:11] # dataset = data.loc[:,'name':'Assembly type'] #this also works
+    # apply index to new accessions dataset
+    dataset.index = dataset.index + last_index + 1
 
 
-# get taxon information
-Taxon = pd.DataFrame()
-errors = pd.DataFrame()
-df_data_list, status_error_list = [], []
-for ind in dataset.index:
-    sample = dataset['sample ID'][ind]
-    value = {'format': 'json', 'fields': 'scientific_name', 'includeAccessions': sample, 'result': 'sample'}
-    r = requests.get('https://www.ebi.ac.uk/ena/portal/api/search', value)
-    if r.status_code == 200:
-        json_data = r.json()
-        df_data = json_normalize(json_data)
-        df_data_list.append(df_data)
-    else:
-        status_code = io.StringIO(str(r.status_code))
-        status_error = pd.read_csv(status_code, names=['status code'])
-        status_error['accession'] = sample
-        status_error_list.append(status_error)
-Taxon = pd.concat(df_data_list, ignore_index=True)
-# subset columns
-Taxon.columns = ['taxon','sample ID']
-Taxon.index = Taxon.index + last_index + 1
+    # get taxon information
+    Taxon = pd.DataFrame()
+    errors = pd.DataFrame()
+    df_data_list, status_error_list = [], []
+    for ind in dataset.index:
+        sample = dataset['sample ID'][ind]
+        value = {'format': 'json', 'fields': 'scientific_name', 'includeAccessions': sample, 'result': 'sample'}
+        r = requests.get('https://www.ebi.ac.uk/ena/portal/api/search', value)
+        if r.status_code == 200:
+            json_data = r.json()
+            df_data = json_normalize(json_data)
+            df_data_list.append(df_data)
+        else:
+            status_code = io.StringIO(str(r.status_code))
+            status_error = pd.read_csv(status_code, names=['status code'])
+            status_error['accession'] = sample
+            status_error_list.append(status_error)
+    Taxon = pd.concat(df_data_list, ignore_index=True)
+    # subset columns
+    Taxon.columns = ['taxon','sample ID']
+    Taxon.index = Taxon.index + last_index + 1
 
-#initial import version (commented out)
-#Taxon.columns = ['sample ID', 'tax_id', 'taxon']
-#Taxon1 = Taxon.drop(['sample ID', 'tax_id'], axis=1)
+    #initial import version (commented out)
+    #Taxon.columns = ['sample ID', 'tax_id', 'taxon']
+    #Taxon1 = Taxon.drop(['sample ID', 'tax_id'], axis=1)
 
-Taxon1 = Taxon.drop(['sample ID'], axis=1)
-dataset1 = dataset.join(Taxon1)
-print(status_error_list)
+    Taxon1 = Taxon.drop(['sample ID'], axis=1)
+    dataset1 = dataset.join(Taxon1)
+    print(status_error_list)
 
-# to import tracking file and duplicate lines for each data type
-# format file with the new accessions to add to the tracking file
-dataset2 = dataset1.loc[dataset1.index.repeat(3),:].reset_index(drop=False)
-dataset2.loc[(dataset2.index == 0), 'accession type'] = 'Contigs'
-dataset2.loc[(dataset2.index % 3 == 0), 'accession type'] = 'Contigs'
-dataset2.loc[(dataset2.index == 1), 'accession type'] = 'Chromosomes'
-dataset2.loc[((dataset2.index + 2) % 3 == 0), 'accession type'] = 'Chromosomes'
-dataset2.loc[(dataset2.index == 2), 'accession type'] = 'GCA'
-dataset2.loc[((dataset2.index + 1) % 3 == 0), 'accession type'] = 'GCA'
-# to add column with data type
-dataset2['accessions'] = np.where(dataset2['accession type'].str.contains("Contigs"), dataset2['Contig range'],
+    # to import tracking file and duplicate lines for each data type
+    # format file with the new accessions to add to the tracking file
+    dataset2 = dataset1.loc[dataset1.index.repeat(3),:].reset_index(drop=False)
+    dataset2.loc[(dataset2.index == 0), 'accession type'] = 'Contigs'
+    dataset2.loc[(dataset2.index % 3 == 0), 'accession type'] = 'Contigs'
+    dataset2.loc[(dataset2.index == 1), 'accession type'] = 'Chromosomes'
+    dataset2.loc[((dataset2.index + 2) % 3 == 0), 'accession type'] = 'Chromosomes'
+    dataset2.loc[(dataset2.index == 2), 'accession type'] = 'GCA'
+    dataset2.loc[((dataset2.index + 1) % 3 == 0), 'accession type'] = 'GCA'
+    # to add column with data type
+    dataset2['accessions'] = np.where(dataset2['accession type'].str.contains("Contigs"), dataset2['Contig range'],
                                   np.where(dataset2['accession type'].str.contains("Chromosomes"), dataset2['Chr range'],
                                            np.where(dataset2['accession type'].str.contains("GCA"), dataset2['GCA ID'], '')))
-# to remove lines with blanks for Chromosomes or Contigs
-dataset2 = dataset2[dataset2['accessions'].str.len() > 0]
-#add version information to the tracking file
-dataset2['version'] = 1
-for ind in dataset2.index:
-    name = dataset2['name'][ind]
-    version = name[name.rfind(".") + 1].split()[0]
-    dataset2['version'][ind]= version
+    # to remove lines with blanks for Chromosomes or Contigs
+    dataset2 = dataset2[dataset2['accessions'].str.len() > 0]
+    #add version information to the tracking file
+    dataset2['version'] = 1
+    for ind in dataset2.index:
+        name = dataset2['name'][ind]
+        version = name[name.rfind(".") + 1].split()[0]
+        dataset2['version'][ind]= version
 
-# to add info on tracking to the file (use Y for all options here with initial import)
-dataset2['Public in ENA'] = "N"
-dataset2['Public in NCBI'] = "N"
-dataset2['Linked to Project'] = "N"
-dataset2['Linked to Sample'] = "N"
-dataset2['publicly available date'] = ""
-# convert columns from string to datetime (step1 only)
-dataset2['submission date'] = pd.to_datetime(dataset2['submission date']).dt.strftime('%d/%m/%Y')
-dataset2['accessioned'] = pd.to_datetime(dataset2['accessioned']).dt.strftime('%d/%m/%Y')
-dataset2['shared to NCBI'] = pd.to_datetime(dataset2['shared to NCBI']).dt.strftime('%d/%m/%Y')
-dataset3 = dataset2.drop(['GCA ID', 'Contig range', 'Chr range'], axis=1)
+    # to add info on tracking to the file (use Y for all options here with initial import)
+    dataset2['Public in ENA'] = "N"
+    dataset2['Public in NCBI'] = "N"
+    dataset2['Linked to Project'] = "N"
+    dataset2['Linked to Sample'] = "N"
+    dataset2['publicly available date'] = ""
+    # convert columns from string to datetime (step1 only)
+    dataset2['submission date'] = pd.to_datetime(dataset2['submission date']).dt.strftime('%d/%m/%Y')
+    dataset2['accessioned'] = pd.to_datetime(dataset2['accessioned']).dt.strftime('%d/%m/%Y')
+    dataset2['shared to NCBI'] = pd.to_datetime(dataset2['shared to NCBI']).dt.strftime('%d/%m/%Y')
+    dataset3 = dataset2.drop(['GCA ID', 'Contig range', 'Chr range'], axis=1)
 
-####################
-##  FILE OUTPUTS  ##
-####################
-# (optional - from initial import script) save new dataset with taxon information added
-#dataset3.to_csv('tracking_file.txt', sep="\t")
+    ####################
+    ##  FILE OUTPUTS  ##
+    ####################
+    # (optional - from initial import script) save new dataset with taxon information added
+    #dataset3.to_csv('tracking_file.txt', sep="\t")
 
-#join the new accessions with existing tracking file
-frames = [tracking, dataset3]
-tracking_new = pd.concat(frames, ignore_index=True)
-#tracking_new = tracking_new.drop(['Unnamed: 0'], axis=1) #not needed...
-
-tracking_new.to_csv(tracking_file_path, sep="\t")
+    #join the new accessions with existing tracking file
+    tracking_new = pd.concat([tracking, dataset3], ignore_index=True)
+    tracking_new.to_csv(tracking_file_path, sep="\t")
+    print(len(dataset1), 'new assemblies added to tracking file with scientific name')
 
--- scripts/assemblytracking/step3_ENA_linking.py
+++ scripts/assemblytracking/step3_ENA_linking.py
@@ -14,12 +14,22 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import io
-import os
-import requests
+import io, os, requests, sys, argparse
 import numpy as np
 import pandas as pd
 from pandas import json_normalize
+
+##################
+##  USER INPUT  ##
+##################
+# set the working directory
+# check the current working directory
+
+parser = argparse.ArgumentParser(description="sql_processingatENA")
+parser.add_argument('-p', '--project', help="Project to track DToL, ASG or ERGA", default="none")
+parser.add_argument('-w', '--workingdir', help="location of tracking file folders",
+                                                default="scripts/assemblytracking/")
+opts = parser.parse_args()
 
 # Portal API function
 def get_links(field,type):
@@ -59,136 +69,140 @@ def get_links(field,type):
         e_range = pd.concat(status_error_list, ignore_index=True)
     return v_range, e_range
 
-##################
-##  USER INPUT  ##
-##################
-# TODO: use argparse function
-
-# set the working directory
-# check the current working directory
-os.getcwd()  # should be 'C:\\Users\\USERNAME\\pathto\\githubrepo\\ena-content-dataflow' on local machine
-# set thw working directory to location of scripts and of config file
-os.chdir('scripts/assemblytracking/')
-# set which project to track - determines the folder where tracking files will be read and written
-project = 'DToL'  # DToL or ASG or ERGA
-# set the location of the tracking files
-tracking_files_path = f'{project}-tracking-files'
-tracking_file_path = f'{tracking_files_path}/tracking_file.txt'
-
-
-###################
-##  FILE INPUTS  ##
-###################
-tracking = pd.read_csv(tracking_file_path, sep='\t',index_col=0)
 
 #############
 ##  MAIN   ##
 #############
 #TODO: check if I'm checking version for chromosomes and GCAs
 
-# base url for portal API
-base_url = 'https://www.ebi.ac.uk/ena/portal/api/links/'
 
-# create sub dataframe with accessions not linked at ENA
-dataset_ENA = tracking[(tracking["Linked to Project"] == "N") | (tracking["Linked to Sample"] == "N")]
-Sample = dataset_ENA['sample ID'].unique()
-Project = dataset_ENA['project'].unique()
+if __name__ == "__main__":
+    # set the working directory
+    # check the current working directory
 
-# Project links to wgs_set
-print("Project links to contigs")
-Project_contigs, Project_contigs_re = get_links(Project, 'wgs_set')
+    os.chdir(opts.workingdir)
+    # set which project to track - determines the folder where tracking files will be read and written
+    project = opts.project  # DToL or ASG or ERGA
+    # set the location of the tracking files
+    tracking_files_path = f'{project}-tracking-files'
+    tracking_file_path = f'{tracking_files_path}/tracking_file.txt'
+    tracking = pd.read_csv(tracking_file_path, sep='\t', index_col=0)
 
-# Project links to chromosomes
-print("Project links to chromosomes")
-Project_chr, Project_chr_re = get_links(Project, 'sequence')
+    # base url for portal API
+    base_url = 'https://www.ebi.ac.uk/ena/portal/api/links/'
 
-Project_chr_range = pd.DataFrame()
-if Project_chr.empty == True:
-    print("no project chromosome links")
-else:
-    Project_chr['accession_last'] = Project_chr['accession']
-    print(Project_chr)
-    aggregation_functions = {'accession': 'min', 'accession_last': 'max'}
-    Project_chr_range = Project_chr.groupby(Project_chr['project/sample ID'], as_index = False).aggregate(aggregation_functions)
-    Project_chr_range['chr_range'] = Project_chr_range['accession'] + "-" + Project_chr_range['accession_last']
-    print(Project_chr_range)
-    # Project_chr_range.to_csv('Project_chr_range.txt', sep="\t") #moved to file outputs section
+    # create sub dataframe with accessions not linked at ENA
+    dataset_ENA = tracking[(tracking["Linked to Project"] == "N") | (tracking["Linked to Sample"] == "N")]
+    Sample = dataset_ENA['sample ID'].unique()
+    Project = dataset_ENA['project'].unique()
 
-# Project links to GCAs
-print("Project links to GCA")
-Project_GCA, Project_GCA_re = get_links(Project, 'assembly')
+    # Project links to wgs_set
+    print("Project links to contigs")
+    Project_contigs, Project_contigs_re = get_links(Project, 'wgs_set')
 
-# Project links to Analysis
-print("Project links to metagenomes")
-Project_analysis, Project_analysis_re = get_links(Project, 'analysis')
+    # Project links to chromosomes
+    print("Project links to chromosomes")
+    Project_chr, Project_chr_re = get_links(Project, 'sequence')
 
-# Sample links to wgs_set
-print("Sample links to contigs")
-Sample_contigs, Sample_contigs_re = get_links(Sample, 'wgs_set')
-
-# Sample links to chromosomes
-print("Sample links to chromosomes")
-Sample_chr, Sample_chr_re = get_links(Sample, 'sequence')
-
-Sample_chr_range = pd.DataFrame()
-if Sample_chr.empty == True:
-    print("no sample chromosome links")
-else:
-    Sample_chr['accession_last'] = Sample_chr['accession']
-    print(Sample_chr)
-    aggregation_functions = {'accession': 'min', 'accession_last': 'max'}
-    Sample_chr_range = Sample_chr.groupby(Sample_chr['project/sample ID'], as_index = False).aggregate(aggregation_functions)
-    Sample_chr_range['chr_range'] = Sample_chr_range['accession'] + "-" + Sample_chr_range['accession_last']
-    print(Sample_chr_range)
-
-# Sample links to GCA
-print("Sample links to GCA")
-Sample_GCA, Sample_GCA_re = get_links(Sample, 'assembly')
-
-# Sample links to Analysis
-print("Sample links to metagenomes")
-Sample_analysis, Sample_analysis_re = get_links(Sample, 'analysis')
-
-# update info on tracking file
-for ind in dataset_ENA.index:
-    if dataset_ENA['Assembly type'][ind] == "primary metagenome" or dataset_ENA['Assembly type'][ind] == "binned metagenome":
-        accession = dataset_ENA['analysis ID'][ind]
-        print(accession)
-        if Project_analysis.empty == False:
-            tracking.loc[:, 'Linked to Project'][ind] = np.where(accession in set(Project_analysis['analysis_accession']), 'Y', 'N')
-        if Sample_analysis.empty == False:
-            tracking.loc[:, 'Linked to Sample'][ind] = np.where(accession in set(Sample_analysis['analysis_accession']), 'Y', 'N')
+    Project_chr_range = pd.DataFrame()
+    if Project_chr.empty == True:
+        print("no project chromosome links")
     else:
-        if dataset_ENA['accession type'][ind] == "Contigs":
-            accession = dataset_ENA['accessions'].str[:8][ind]
-            print(accession)
-            if Project_contigs.empty == False:
-                tracking.loc[:, 'Linked to Project'][ind] = np.where(accession in set(Project_contigs['accession'].str[:8]), 'Y', 'N')
-            if Sample_contigs.empty == False:
-                tracking.loc[:, 'Linked to Sample'][ind] = np.where(accession in set(Sample_contigs['accession'].str[:8]), 'Y', 'N')
-        if dataset_ENA['accession type'][ind] == "GCA":
-            accession = dataset_ENA['accessions'][ind]
-            print(accession)
-            if Project_GCA.empty == False:
-                tracking.loc[:, 'Linked to Project'][ind] = np.where(accession in set(Project_GCA['accession']), 'Y', 'N')
-            if Sample_GCA.empty == False:
-                tracking.loc[:, 'Linked to Sample'][ind] = np.where(accession in set(Sample_GCA['accession']), 'Y', 'N')
-        if dataset_ENA['accession type'][ind] == "Chromosomes":
-            accession = dataset_ENA['accessions'][ind]
-            print(accession)
-            if Project_chr.empty == False:
-                tracking.loc[:, 'Linked to Project'][ind] = np.where(accession in set(Project_chr_range['chr_range']), 'Y', 'N')
-            if Sample_chr.empty == False:
-                tracking.loc[:, 'Linked to Sample'][ind] = np.where(accession in set(Sample_chr_range['chr_range']), 'Y', 'N')
-print(tracking)
+        Project_chr['accession_last'] = Project_chr['accession']
+        print(Project_chr)
+        aggregation_functions = {'accession': 'min', 'accession_last': 'max'}
+        Project_chr_range = Project_chr.groupby(Project_chr['project/sample ID'], as_index=False).aggregate(
+            aggregation_functions)
+        Project_chr_range['chr_range'] = Project_chr_range['accession'] + "-" + Project_chr_range['accession_last']
+        print(Project_chr_range)
+        # Project_chr_range.to_csv('Project_chr_range.txt', sep="\t") #moved to file outputs section
 
-####################
-##  FILE OUTPUTS  ##
-####################
+    # Project links to GCAs
+    print("Project links to GCA")
+    Project_GCA, Project_GCA_re = get_links(Project, 'assembly')
 
-# save updated tracking file
-tracking.to_csv(f'{tracking_files_path}/tracking_file.txt', sep="\t")
+    # Project links to Analysis
+    print("Project links to metagenomes")
+    Project_analysis, Project_analysis_re = get_links(Project, 'analysis')
 
-# save additional output(s)
-Project_chr_range.to_csv(f'{tracking_files_path}/Project_chr_range.txt', sep="\t")
-#Project_links_re.to_csv('project_links_errors.txt', sep="\t")
+    # Sample links to wgs_set
+    print("Sample links to contigs")
+    Sample_contigs, Sample_contigs_re = get_links(Sample, 'wgs_set')
+
+    # Sample links to chromosomes
+    print("Sample links to chromosomes")
+    Sample_chr, Sample_chr_re = get_links(Sample, 'sequence')
+
+    Sample_chr_range = pd.DataFrame()
+    if Sample_chr.empty == True:
+        print("no sample chromosome links")
+    else:
+        Sample_chr['accession_last'] = Sample_chr['accession']
+        print(Sample_chr)
+        aggregation_functions = {'accession': 'min', 'accession_last': 'max'}
+        Sample_chr_range = Sample_chr.groupby(Sample_chr['project/sample ID'], as_index=False).aggregate(
+            aggregation_functions)
+        Sample_chr_range['chr_range'] = Sample_chr_range['accession'] + "-" + Sample_chr_range['accession_last']
+        print(Sample_chr_range)
+
+    # Sample links to GCA
+    print("Sample links to GCA")
+    Sample_GCA, Sample_GCA_re = get_links(Sample, 'assembly')
+
+    # Sample links to Analysis
+    print("Sample links to metagenomes")
+    Sample_analysis, Sample_analysis_re = get_links(Sample, 'analysis')
+
+    # update info on tracking file
+    for ind in dataset_ENA.index:
+        if dataset_ENA['Assembly type'][ind] == "primary metagenome" or dataset_ENA['Assembly type'][
+            ind] == "binned metagenome":
+            accession = dataset_ENA['analysis ID'][ind]
+            print(accession)
+            if Project_analysis.empty == False:
+                tracking.loc[:, 'Linked to Project'][ind] = np.where(
+                    accession in set(Project_analysis['analysis_accession']), 'Y', 'N')
+            if Sample_analysis.empty == False:
+                tracking.loc[:, 'Linked to Sample'][ind] = np.where(
+                    accession in set(Sample_analysis['analysis_accession']), 'Y', 'N')
+        else:
+            if dataset_ENA['accession type'][ind] == "Contigs":
+                accession = dataset_ENA['accessions'].str[:8][ind]
+                print(accession)
+                if Project_contigs.empty == False:
+                    tracking.loc[:, 'Linked to Project'][ind] = np.where(
+                        accession in set(Project_contigs['accession'].str[:8]), 'Y', 'N')
+                if Sample_contigs.empty == False:
+                    tracking.loc[:, 'Linked to Sample'][ind] = np.where(
+                        accession in set(Sample_contigs['accession'].str[:8]), 'Y', 'N')
+            if dataset_ENA['accession type'][ind] == "GCA":
+                accession = dataset_ENA['accessions'][ind]
+                print(accession)
+                if Project_GCA.empty == False:
+                    tracking.loc[:, 'Linked to Project'][ind] = np.where(accession in set(Project_GCA['accession']),
+                                                                         'Y', 'N')
+                if Sample_GCA.empty == False:
+                    tracking.loc[:, 'Linked to Sample'][ind] = np.where(accession in set(Sample_GCA['accession']), 'Y',
+                                                                        'N')
+            if dataset_ENA['accession type'][ind] == "Chromosomes":
+                accession = dataset_ENA['accessions'][ind]
+                print(accession)
+                if Project_chr.empty == False:
+                    tracking.loc[:, 'Linked to Project'][ind] = np.where(
+                        accession in set(Project_chr_range['chr_range']), 'Y', 'N')
+                if Sample_chr.empty == False:
+                    tracking.loc[:, 'Linked to Sample'][ind] = np.where(accession in set(Sample_chr_range['chr_range']),
+                                                                        'Y', 'N')
+    print(tracking)
+
+    ####################
+    ##  FILE OUTPUTS  ##
+    ####################
+
+    # save updated tracking file
+    tracking.to_csv(f'{tracking_files_path}/tracking_file.txt', sep="\t")
+
+    # save additional output(s)
+    Project_chr_range.to_csv(f'{tracking_files_path}/Project_chr_range.txt', sep="\t")
+    # Project_links_re.to_csv('project_links_errors.txt', sep="\t")
+
